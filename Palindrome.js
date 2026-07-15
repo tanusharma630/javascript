6477803bc7ee2e91abff97ea6e726263a73be197
tanusharma630 <tanusharma2313@gmail.com>
2026-07-15 14:40:08 +0530

    project done

---
+++ Palindrome.js
@@ -0,0 +1,19 @@
+const prompt = require("prompt-sync")();
+const userString = prompt("Enter Your String : ")
+let str = '';
+
+
+if(!userString){
+    console.log("Invalid String")
+}
+
+
+for(let i=userString.length-1; i>=0; i--){
+    str += userString[i]
+}
+
+if(userString===str){
+    console.log("isPalindrom")
+}else{
+    console.log("isnotPalindrom")
+}
